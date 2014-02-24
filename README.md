b8ff25d255039bbc44e98465ac4e26c755b20be0
Slaman <jordan.slaman@gmail.com>
2014-02-23 19:07:28 -0500

    Readme update, build works but instructions still unclear.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # anchorcms-docker
 
-wip, build failing.
-Do not use.
+A Dockerfile that sets up a LEMP stack running AnchorCMS
+
+Modified from Eugene Ware's docker-wordpress-nginx script: https://github.com/eugeneware/docker-wordpress-nginx/
+
+## Installation
+'''
+git clone https://github.com/JordanSlaman/anchorcms-docker.git
+cd anchorcms-docker
+sudo docker build -t="anchorcms-docker" .
+'''
+
+## Usage
+Run the container:
+'''
+sudo docker run -p=80:80 -d anchorcms-docker /bin/bash /start.sh
+'''
+
+Stopping the container:
+find the ID with 'sudo docker ps'
+run 'sudo docker stop $ID'
+
+Modifying the container:
+'''
+docker run -p=80:80 -i -t anchorcms-docker /bin/bash
+'''
+
+MySQL root password is located in a text file in '/mysql-root-pw.txt' after start runs.
--- start.sh
+++ start.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-#mysql has to be started this way as it doesn't work to call from /etc/init.d
-/usr/bin/mysqld_safe & 
-sleep 10s
-# Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
-ANCHOR_DB="anchor"
-MYSQL_PASSWORD=`pwgen -c -n -1 12`
-ANCHOR_PASSWORD=`pwgen -c -n -1 12`
-#This is so the passwords show up in logs. 
-echo mysql root password: $MYSQL_PASSWORD
-echo mysql anchorcms password: $ANCHOR_PASSWORD
-echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-echo $ANCHOR_PASSWORD > /anchor-db-pw.txt
+#If you'd preffer to use the anchor user than the root for mysql uncomment the anchor password lines.
 
-mysqladmin -u root password $MYSQL_PASSWORD 
-mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE anchor; GRANT ALL PRIVILEGES ON anchor.* TO 'anchor'@'localhost' IDENTIFIED BY '$ANCHOR_PASSWORD'; FLUSH PRIVILEGES;"
-killall mysqld
+if [ ! -f /mysql-root-pw.txt ]; then
+  #mysql has to be started this way as it doesn't work to call from /etc/init.d
+  /usr/bin/mysqld_safe & 
+  sleep 10s
 
+  # Here we generate random passwords (thank you pwgen!). 
+  MYSQL_PASSWORD=`pwgen -c -n -1 12`
+  #ANCHOR_PASSWORD=`pwgen -c -n -1 12`
+
+  #This is so the passwords show up in logs. 
+  echo mysql root password: $MYSQL_PASSWORD
+  #echo mysql anchorcms password: $ANCHOR_PASSWORD
+  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
+  #echo $ANCHOR_PASSWORD > /anchor-db-pw.txt
+
+  mysqladmin -u root password $MYSQL_PASSWORD 
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE anchor; GRANT ALL PRIVILEGES ON anchor.* TO 'anchor'@'localhost' IDENTIFIED BY '$ANCHOR_PASSWORD'; FLUSH PRIVILEGES;"
+  killall mysqld
+fi
 # start all the services
 /usr/local/bin/supervisord -n
